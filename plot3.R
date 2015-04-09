ca1dcdc60863eb7c655727db2aee8bb2771b9d00
Smintie007 <linda.bruwer@gmail.com>
2015-04-09 16:22:30 +0200

    plot3

---
+++ plot3.R
@@ -0,0 +1,39 @@
+
+## Read dataset into data frame called hpc
+hpc <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02")) ## store subset date range in variable data
+rm(hpc)					## remove data frame hpc
+
+## Converting dates
+fulldate <- paste(as.Date(data$Date), data$Time)
+data$Full_Date <- as.POSIXct(fulldate)
+## print(head(data))                used to check format comment out for run
+
+## Plot Graph of Global Active Power
+## y-axis label "Global Active Power"
+## x-axis weekdays
+
+par(mar = c(3, 5, 2, 4),cex = 0.9)  # margin around the plot bottom,left.top,right
+# par(cex.lab = 0.8 )
+# par(cex.axis = 0.8 )
+# par(cex = 0.9 )  # font size relative to current
+
+with(data, {
+  plot(Sub_metering_1~Full_Date, type="l", ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Full_Date,col='Red')   ## 
+  lines(Sub_metering_3~Full_Date,col='Blue')
+})
+
+## creating legend
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1,
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.9,)
+
+
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()  ## close plot3.png
+
+## dev.copy(png, file="plot3.png", height=480, width=640 ,units = "px", pointsize = 8,  bg = "white")
