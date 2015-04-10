4c773d92b66241aeef39414d06e4c43ecb14c0ce
Smintie007 <linda.bruwer@gmail.com>
2015-04-10 11:57:46 +0200

    amend comments

--- plot4.R
+++ plot4.R
@@ -36,4 +36,4 @@ with(data, {
 dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()  ## close plot4.png
 
-## dev.copy(png, file="plot3.png", height=480, width=640 ,units = "px", pointsize = 8,  bg = "white")
+
